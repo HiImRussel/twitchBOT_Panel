9656cea756bee86aa94884b02c469475ec804de3
Russel <russel1234561@gmail.com>
2021-07-22 19:03:59 +0200

    Added call to server to check sr status

--- src/app/components/SongRequest/song-request/song-request.component.ts
+++ src/app/components/SongRequest/song-request/song-request.component.ts
@@ -1,15 +1,26 @@
+import { UserService } from 'src/app/services/user.service';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-song-request',
   templateUrl: './song-request.component.html',
-  styleUrls: ['./song-request.component.scss']
+  styleUrls: ['./song-request.component.scss'],
 })
 export class SongRequestComponent implements OnInit {
+  isOn = false;
 
-  constructor() { }
+  constructor(private http: HttpClient, private user: UserService) {}
 
   ngOnInit(): void {
+    this.http
+      .post('http://localhost:3000/checkSr', {
+        id: this.user.userData.id,
+      })
+      .subscribe((data: any) => {
+        if (data.status === 'ok') {
+          this.isOn = data.srStatus;
+        }
+      });
   }
-
 }
